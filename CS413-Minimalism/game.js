ef792e0b19fa624500265c2728f5e843d664712f
スーツ芋 <Erekyth@yahoo.com>
2016-05-16 09:01:37 -0700

    game.js comments
    Added comments to the Javascript code

--- CS413-Minimalism/game.js
+++ CS413-Minimalism/game.js
@@ -1,8 +1,14 @@
+// Attach to the HTML document via gameport ID
 var gameport = document.getElementById("gameport");
 
+// Create the Renderer
+// 400x400 pixel with a black background
 var renderer = PIXI.autoDetectRenderer(400, 400, {BackgroundColor: 0x3344ee});
+
+// Apply to the HTML view
 gameport.appendChild(renderer.view);
 
+// Create the stage using the Container object
 var stage = new PIXI.Container();
 
 var texture = PIXI.Texture.fromImage("SmallBlock.png")
@@ -20,6 +26,8 @@ stage.addChild(sprite);
 function animate() {
 	requestAnimationFrame(animate);
 	sprite.rotation += 0.1;
+	
+	// Renderer 'renders' the stage.
 	renderer.render(stage);
 }
 
